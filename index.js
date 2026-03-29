d94f2bc37245e54d7c37d8dff7465128a83646ee
sarngastone <sarngastone@gmail.com>
2026-03-30 09:47:41 +1000

    added functions for basic math operations

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(num1, num2) {
+    let sum = num1 + num2
+    return sum
+}
+
+function subtract(num1, num2) {
+    let sum = num1 - num2
+    return sum
+}
+function multiply(num1, num2) {
+    let sum = num1 * num2
+    return sum
+}
+function divide(num1, num2) {
+    let sum = num1 / num2
+    return sum
+}
+console.log(divide(20, 3))
